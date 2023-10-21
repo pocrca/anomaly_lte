5c8cc6448d027fc6fe3a8a85b9235754b3e5cef4
Brayden Chang <braydenchangjy@gmail.com>
2023-10-21 10:06:53 +0000

    Added Data Preparation files

---
+++ src/Data_preparation.py
@@ -0,0 +1,18 @@
+#Importing libraries
+import pandas as pd 
+import numpy as np 
+from sklearn.model_selection import train_test_split
+
+#Splitting data
+df = pd.read_csv("/workspace/anomaly_lte/data/train.csv", sep = ';')
+
+X = df[df.columns[0:13]]
+Y = df[df.columns[13]]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state = 53, test_size = 0.2, shuffle = True)
+
+#Exporting dataframes to csv files
+X_train.to_csv('X_train.csv')
+X_test.to_csv('X_test.csv')
+Y_train.to_csv('Y_train.csv')
+Y_test.to_csv('Y_test.csv')
