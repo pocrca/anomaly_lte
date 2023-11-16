c2f8c14e33e75f46c5f4fdbbef6ea6282112f0a9
Brayden Chang <braydenchangjy@gmail.com>
2023-11-16 04:18:43 +0000

    Updated decision trees and added xgboost

--- src/train_decisiontree_ft1.py
+++ src/train_decisiontree_ft1.py
@@ -14,55 +14,25 @@ Y_df = pd.read_csv("/workspace/anomaly_lte/data/Y_train.csv")
 X_df = X_df.iloc[:,1:]
 Y_df = Y_df.iloc[:,1:]
 
-# Creating classifier
-classifier = DecisionTreeClassifier(random_state=53)
+# Creating DecisionTree Classifiers
+for fold in range(1,6):
+    globals()[f"dtree_ft1_fold{fold}"] = DecisionTreeClassifier(random_state=53)
 
 # Creating Stratified K-Fold
 skf = StratifiedKFold(n_splits=5, shuffle = True, random_state = 53)
 
-# Fitting Model
-scores = []
-AUCs = []
-Fold_number = 0
-Mean_cm = [[0,0],[0,0]]
+# Fitting Models
+Fold = 0
 
 for train_index, test_index in skf.split(X_df, Y_df):
+    Fold += 1
     X_train, X_test = X_df.iloc[train_index], X_df.iloc[test_index],
     Y_train, Y_test = Y_df.iloc[train_index], Y_df.iloc[test_index],
-    classifier.fit(X_train, Y_train)
-    Predicted = classifier.predict(X_test)
+    globals()[f"dtree_ft1_fold{Fold}"].fit(X_train, Y_train)
+    with open(f'models/train_dtree_ft1.pkl', 'wb') as model_file:
+        pickle.dump(dtree_ft1_fold1, model_file)
+        pickle.dump(dtree_ft1_fold2, model_file)
+        pickle.dump(dtree_ft1_fold3, model_file)
+        pickle.dump(dtree_ft1_fold4, model_file)
+        pickle.dump(dtree_ft1_fold5, model_file)
 
-    # Getting Scores
-    score = classifier.score(X_test,Y_test)
-    scores.append(score)
-
-    # Getting confusion matrix
-    cm = confusion_matrix(Y_test, Predicted)
-    Mean_cm = Mean_cm + cm 
-
-    # Getting ROC Curve
-    fpr, tpr, thresholds = roc_curve(Y_test, Predicted)
-
-    # Getting AUC values
-    AUC = auc(fpr, tpr)
-    AUCs.append(AUC)
-
-    # Plotting ROC Curve
-    Fold_number += 1
-    plt.plot(fpr, tpr, label = f"ROC Curve | AUC = {round(AUC,3)}, Score = {round(score,3)}")
-    plt.plot([0,1], [0,1], linestyle = '--', label = "Baseline")
-    plt.title(f"Fold {Fold_number}")
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.grid()
-    plt.legend()
-    plt.show()
-
-# Printing overall evaluation metrics
-print(f"Mean Score: {np.mean(scores)}")
-print(f"Mean AUC: {np.mean(AUCs)}")
-print(f"Mean Confusion Matrix: \n{Mean_cm/5}")
-
-# Saving Models as pickle files
-with open('train_dtree_ft1.pkl', 'wb') as model_file:
-    pickle.dump(classifier, model_file)
--- src/train_xgboost_ft1.py
+++ src/train_xgboost_ft1.py
@@ -3,12 +3,12 @@ import numpy as np
 import pandas as pd 
 import pickle
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import roc_curve, auc, confusion_matrix
 
 # Creating DataFrames and removing index created during feature_engineering export
-X_df = pd.read_csv("/workspace/anomaly_lte/data/x_train_processed_ft2.csv")
+X_df = pd.read_csv("/workspace/anomaly_lte/data/x_train_processed_ft1.csv")
 Y_df = pd.read_csv("/workspace/anomaly_lte/data/Y_train.csv")
 
 X_df = X_df.iloc[:,1:]
@@ -16,7 +16,7 @@ Y_df = Y_df.iloc[:,1:]
 
 # Creating DecisionTree Classifiers
 for fold in range(1,6):
-    globals()[f"dtree_ft2_fold{fold}"] = DecisionTreeClassifier(random_state=53)
+    globals()[f"xgboost_ft1_fold{fold}"] = XGBClassifier(random_state=53)
 
 # Creating Stratified K-Fold
 skf = StratifiedKFold(n_splits=5, shuffle = True, random_state = 53)
@@ -28,10 +28,10 @@ for train_index, test_index in skf.split(X_df, Y_df):
     Fold += 1
     X_train, X_test = X_df.iloc[train_index], X_df.iloc[test_index],
     Y_train, Y_test = Y_df.iloc[train_index], Y_df.iloc[test_index],
-    globals()[f"dtree_ft2_fold{Fold}"].fit(X_train, Y_train)
-    with open(f'models/train_dtree_ft2.pkl', 'wb') as model_file:
-        pickle.dump(dtree_ft2_fold1, model_file)
-        pickle.dump(dtree_ft2_fold2, model_file)
-        pickle.dump(dtree_ft2_fold3, model_file)
-        pickle.dump(dtree_ft2_fold4, model_file)
-        pickle.dump(dtree_ft2_fold5, model_file)
+    globals()[f"xgboost_ft1_fold{Fold}"].fit(X_train, Y_train)
+    with open(f'models/train_xgboost_ft1.pkl', 'wb') as model_file:
+        pickle.dump(xgboost_ft1_fold1, model_file)
+        pickle.dump(xgboost_ft1_fold2, model_file)
+        pickle.dump(xgboost_ft1_fold3, model_file)
+        pickle.dump(xgboost_ft1_fold4, model_file)
+        pickle.dump(xgboost_ft1_fold5, model_file)
